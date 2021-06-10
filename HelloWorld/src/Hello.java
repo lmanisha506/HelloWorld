e43c5e3e18c61ec89aa1af90b04dd83f8aec2f64
Raju K C <Raju K C@DESKTOP-AGEKBS0>
2021-06-10 13:09:43 -0500

    First commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Hello {  
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al=new ArrayList<>();
+		al.add(2);
+		al.add(3);
+		System.out.println(al);
+		al.remove(0);
+		System.out.println(al);    
+		
+     
+	} 
+ 
+}
